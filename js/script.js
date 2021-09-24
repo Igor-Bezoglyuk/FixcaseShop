3b6f9c88aeae892e5b0b05189eb4f7c441ea1b57
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2021-09-25 02:54:46 +0300

    new script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+jQuery( document ).ready(function(){
+    $(function (){
+    $("#Open").click(function(){
+    $(".header-modal-back").toggle(400); 
+ });
+});
+});
 
+jQuery(document).ready(function(){
+	$(function(){
+	$('.popular').on('click', function(){
+		$('.Kontent3-blocks-carts').css('display', 'inline-block');
+	});
+	$('.skidka').on('click', function(){
+		$('.novinka-block').css('display', 'none');
+		$('.skidka-block').css('display', 'inline-block');
+	});
+	$('.Novika').on('click', function(){
+		$('.skidka-block').css('display', 'none');
+		$('.novinka-block').css('display', 'inline-block');
+	});
+});
+});
